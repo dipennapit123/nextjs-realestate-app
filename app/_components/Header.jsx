4d29203c603dbc591cc814b9799b6753783ae39b
dipennapit123 <dipennapit45@gmail.com>
2024-07-17 18:45:26 +0545

    Authentication using clerk

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { UserButton, useUser } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
@@ -8,20 +8,23 @@ import { usePathname } from "next/navigation";
 import React, { useEffect } from "react";
 
 function Header() {
-    const path=usePathname();
-    const{user,isSignedIn}=useUser();
+  const path = usePathname();
+  const { user, isSignedIn } = useUser();
 
-
-    useEffect(()=>{
-        console.log(path)
-    },[])
+  useEffect(() => {
+    console.log(path);
+  }, []);
   return (
     <div className="p-6 px-10 flex justify-between shadow-sm fixed top-0 w-full z-10 bg-white">
       <div className="flex gap-12 items-center">
         <Image src={"/logo.svg"} width={150} height={150} alt="logo" />
         <ul className="hidden md:flex gap-10">
           <Link href={"/"}>
-            <li className={`"hover:text-primary font-medium text-sm cursor-pointer" ${path=='/' && 'text-primary'}`}>
+            <li
+              className={`"hover:text-primary font-medium text-sm cursor-pointer" ${
+                path == "/" && "text-primary"
+              }`}
+            >
               For Sale
             </li>
           </Link>
@@ -33,15 +36,18 @@ function Header() {
           </li>
         </ul>
       </div>
-      <div className="flex gap-2 items-start">
+      <div className="flex gap-2 items-center">
         <Button className="flex gap-2">
           <Plus className="h-5 w-5" />
           Post Your Ad
         </Button>
-        {isSignedIn ?
-        <UserButton/>
-        :<Button variant="outline">Login</Button>
-        }
+        {isSignedIn ? (
+          <UserButton />
+        ) : (
+          <Link href={"/sign-in"}>
+            <Button variant="outline">Login</Button>
+          </Link>
+        )}
       </div>
     </div>
   );
