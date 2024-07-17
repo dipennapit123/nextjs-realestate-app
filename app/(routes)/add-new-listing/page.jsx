6d6ab3a61e4a3935d67c65295ab7032a434241c8
dipennapit123 <dipennapit45@gmail.com>
2024-07-17 22:46:37 +0545

    adding google cordinate when button is clicked and disable button when location is not the

--- app/(routes)/add-new-listing/page.jsx
+++ app/(routes)/add-new-listing/page.jsx
@@ -1,22 +1,33 @@
-import GoogleAddressSearch from '@/app/_components/GoogleAddressSearch'
-import { Button } from '@/components/ui/button'
-import React from 'react'
-
+'use client'
+import GoogleAddressSearch from "@/app/_components/GoogleAddressSearch";
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
 
 function AddNewListing() {
-  return (
-    <div className='mt-10 md:mx-56 lg:mx-80 '>
+  const [selectedAddress, setSelectedAddress] = useState();
+  const [coordinates, setCoordinates] = useState();
 
-    <div className='p-10 flex flex-col gap-5 items-center justify-center'>
-      <h2 className='font-bold text-2xl '>Add New Listing</h2>
-      <div className='p-10 px-28 w-full rounded-lg border shadow-md flex flex-col gap-5'>
-        <h2 className='text-gray-500'>Enter Addres which you want to list</h2>
-        <GoogleAddressSearch/>
-        <Button>Next</Button>
+  const nextHandler=()=>{
+    console.log(selectedAddress,coordinates)
+  }
+
+  return (
+    <div className="mt-10 md:mx-56 lg:mx-80 ">
+      <div className="p-10 flex flex-col gap-5 items-center justify-center">
+        <h2 className="font-bold text-2xl ">Add New Listing</h2>
+        <div className="p-10 w-full rounded-lg border shadow-md flex flex-col gap-5">
+          <h2 className="text-gray-500">Enter Addres which you want to list</h2>
+          <GoogleAddressSearch
+            selectedAddress={(value) => setSelectedAddress(value)}
+            setCoordinates={(value) => setCoordinates(value)}
+          />
+          <Button
+          disabled={!selectedAddress || !coordinates}
+          onClick={nextHandler}>Next</Button>
+        </div>
       </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default AddNewListing
+export default AddNewListing;
--- app/_components/GoogleAddressSearch.jsx
+++ app/_components/GoogleAddressSearch.jsx
@@ -1,9 +1,12 @@
 "use client";
 import { MapPin } from "lucide-react";
 import React from "react";
-import GooglePlacesAutocomplete, { geocodeByAddress, getLatLng } from "react-google-places-autocomplete";
+import GooglePlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-google-places-autocomplete";
 
-function GoogleAddressSearch() {
+function GoogleAddressSearch({selectedAddress,setCoordinates}) {
   return (
     <div className="flex items-center w-full">
       <MapPin className="h-10 w-10 p-2 rounded-l-lg text-primary bg-purple-200" />
@@ -15,11 +18,13 @@ function GoogleAddressSearch() {
           isClearable: true,
           className: "w-full",
           onChange: (place) => {
-            console.log(place);
-            geocodeByAddress(place.label).then(result=>getLatLng(result[0]))
-            .then(({lat,lng})=>{
-                console.log(lat,lng)
-            })
+        
+            selectedAddress(place)
+            geocodeByAddress(place.label)
+              .then((result) => getLatLng(result[0]))
+              .then(({ lat, lng }) => {
+                setCoordinates({ lat, lng });
+              });
           },
         }}
       />
